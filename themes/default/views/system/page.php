b8dcc56f2df6ad87d6e4bc1b81b850ac16ce42e7
MariyaPuzyr <mariyapyzur@gmail.com>
2020-07-21 15:27:42 +0300

    fix all text pages

--- themes/default/views/system/page.php
+++ themes/default/views/system/page.php
@@ -2,15 +2,13 @@
     $this->pageTitle = Yii::t('core', 'page_'.$model->id);
     $text_field = 'text_'.Yii::app()->language;
 ?>
-<div class="container">
-    <div class="row bg-white p-3 mt-3">
-        <div class="col-md-12 text-center">
-            <h4><?= Yii::t('core', 'page_'.$model->id); ?></h4>
-        </div>
+<div class="row">
+  <div class="col-lg-12">
+    <div class="card">
+      <div class="card-body with-list-arrow">
+          <h4 class="card-title"><?= Yii::t('core', 'page_'.$model->id); ?></h4>
+          <?= $model->{$text_field ? $text_field : 'text_ru'}; ?>
+      </div>
     </div>
-    <div class="row bg-white p-3">
-        <div class="col-md-12 text-justify">
-            <?= $model->{$text_field ? $text_field : 'text_ru'}; ?>
-        </div>
-    </div>
+  </div>
 </div>
