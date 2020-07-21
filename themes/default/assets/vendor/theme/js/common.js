cacc2834e1ed0c191f364c238dcfb4bfe2501d6c
MariyaPuzyr <mariyapyzur@gmail.com>
2020-07-21 13:16:17 +0300

    remove #app-side script

--- themes/default/assets/vendor/theme/js/common.js
+++ themes/default/assets/vendor/theme/js/common.js
@@ -6,38 +6,6 @@
  */
 'use strict';
 
-/* eslint-disable no-undef */
-$(function () {
-    if ($(window).width() < 992) {
-        $('#app-side').onoffcanvas('hide');
-    } else {
-        $('#app-side').onoffcanvas('show');
-    }
-
-    $('.side-nav .unifyMenu').unifyMenu({toggle: true});
-
-    $('#app-side-hoverable-toggler').on('click', function () {
-        $('.app-side').toggleClass('is-hoverable');
-        $(undefined).children('i.fa').toggleClass('fa-angle-right fa-angle-left');
-    });
-
-    $('#app-side-mini-toggler').on('click', function () {
-        $('.app-side').toggleClass('is-mini');
-        $("#app-side-mini-toggler i").toggleClass('icon-chevron-thin-left icon-chevron-thin-right');
-    });
-
-    $('#onoffcanvas-nav').on('click', function () {
-        $('.app-side').toggleClass('left-toggle');
-        $('.app-main').toggleClass('left-toggle');
-        $("#onoffcanvas-nav i").toggleClass('icon-chevron-thin-left icon-chevron-thin-right');
-    });
-
-    $('.onoffcanvas-toggler').on('click', function () {
-        $(".onoffcanvas-toggler i").toggleClass('icon-chevron-thin-left icon-chevron-thin-right');
-    });
-});
-
-
 // Bootstrap Tooltip
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
